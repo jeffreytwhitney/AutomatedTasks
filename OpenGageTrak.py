edf0ed50887105343460d6071b80ae36a5ec4f2a
jeffreytwhitney <jeffrey.t.whitney@gmail.com>
2024-05-06 06:03:54 -0500

    Uprev CMM Baby!

--- OpenGageTrak.py
+++ OpenGageTrak.py
@@ -5,7 +5,7 @@ import pyautogui
 
 os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\GAGEtrak 6.8\\GAGEtrak 6.8.lnk")
 time.sleep(4)
-pyautogui.typewrite("J. Whitney")
+pyautogui.typewrite("Shop")
 pyautogui.typewrite("\t")
-pyautogui.typewrite("An0nym0us!")
+pyautogui.typewrite("1234")
 pyautogui.press('enter')
--- RunCurl.py
+++
@@ -1,7 +0,0 @@
-from tkinter import messagebox
-
-import RunPowershell
-
-RunPowershell.run_powershell_script("C:\\Users\\Public\\CURL\\bin\\CMM.ps1")
-
-messagebox.showinfo("Hello", f"Hi!")
