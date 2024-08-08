27a1d7437cdd539c4990764434accb25faa77e9f
jeffreytwhitney <jeffrey.t.whitney@gmail.com>
2024-08-08 05:49:14 -0500

    Move MicroVu to 1Factory.

--- MoveMicroVuFolderTo1Factory.py
+++ MoveMicroVuFolderTo1Factory.py
@@ -1,15 +1,15 @@
 import os
+import shutil
 import sys
 from tkinter import messagebox
 
 import ArchiveLib
+import FolderArchiveLib
 
 
 def run_test():
-    sub_directory = ArchiveLib.GetMicroVuFolderSubDirectory("X:\\Quality Calibration\\Work in Progress\\Jeffrey\\1Factory Conversion\\311\\JP100153081_REV C")
-    dirname = \
-        os.path.dirname("X:\\Quality Calibration\\Work in Progress\\Jeffrey\\1Factory Conversion\\311\\JP100153081_REV C\\").split("\\")[
-            -1]
+    dir_text = "X:\\Quality Calibration\\Work in Progress\\Jeffrey\\1Factory Conversion\\311\\JP100153081_REV C"
+    dirname = os.path.basename(dir_text)
     messagebox.showinfo("Hi!", dirname)
     return
 
@@ -19,21 +19,18 @@ n = len(sys.argv)
 if n == 1:
     run_test()
 else:
-
+    output_directory = ""
     for i in range(1, n):
         machine_directory = ArchiveLib.GetMicroVuFolderSubDirectory(sys.argv[i])
-        output_directory = ""
-        dirname = os.path.dirname(sys.argv[i]).split("\\")[-1]
-        messagebox.showinfo("Hi!", dirname)
+        dirname = os.path.basename(sys.argv[i])
 
-        # if machine_directory == "311":
-        #     dirname = os.path.dirname(sys.argv[i])
-
-        # if os.path.exists(output_directory):
-        #     if user_response := messagebox.askyesno("Are You Sure?", f"Directory '{output_directory}' already exists. Overwrite?"):
-        #         shutil.rmtree(output_directory)
-        #     else:
-        #         exit()
-        # shutil.copytree(source_directory, output_directory)
-
-        exit()
+        output_directory = f"V:\\Inspect Programs\\Micro-Vu\\Approved Programs\\{machine_directory}\\1Factory\\{dirname}"
+        if os.path.exists(output_directory):
+            if user_response := messagebox.askyesno("Are You Sure?", f"Directory '{output_directory}' already exists. Archive and overwrite?"):
+                FolderArchiveLib.CopyFolderToSubDirectoryWithFileNameAppend(
+                    output_directory, machine_directory, "ArchiveMicroVuFolder", False)
+                shutil.rmtree(output_directory)
+            else:
+                continue
+        shutil.copytree(sys.argv[i], output_directory)
+        shutil.rmtree(sys.argv[i])
